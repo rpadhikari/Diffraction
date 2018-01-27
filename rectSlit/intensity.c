3f3121c25554b143d08a729a1df1dc262e40c111
Rajendra Adhikari <rpadhikari@gmail.com>
2018-01-28 00:07:45 +0545

    Intensity normalized to unity

--- rectSlit/intensity.c
+++ rectSlit/intensity.c
@@ -7,11 +7,12 @@ double pow(double, double); // for x^y we use pow(x, y)
  * a = slit width in mm
 */
 // main function definition for calculating intensity
-double intensity(double lambda, double a, double theta) {
-  double I0 = 1.0; //Maximum intensity
+double intensity(double alpha) {
+//  double I0 = 1.0; // Maximum intensity set to unity
   double I; // intensity to be computed
   double pi=3.14159; // value of pi, not done in higher percision
-  I=I0*pow(sin(pi*a*sin(theta)/lambda)/(pi*a*sin(theta)/lambda),2.0);
+//  double alpha = a*sin(theta)/lambda;
+//  I=I0*pow(sin(pi*a*sin(theta)/lambda)/(pi*a*sin(theta)/lambda),2.0);
+  I=pow(sin(pi*alpha)/(pi*alpha), 2.0);
   return I;
 }
-
